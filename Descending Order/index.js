e38163e65c67aef41cfd09b36045e0ea3a7da707
DmitryKRTV <dmitrykorotaev.job@gmail.com>
2022-12-06 20:11:13 +0300

    Initial commit

---
+++ Descending Order/index.js
@@ -0,0 +1,9 @@
+const courses1 = [
+    { title: "css", price: 110 },
+    { title: "js", price: 200 },
+    { title: "react", price: 150 },
+]
+
+const courses2 = courses1.map(i => i.price + 100)
+
+console.log(courses2)
