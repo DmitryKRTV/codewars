b6eb714bbf94046a4eb484037091e0e9b6f03448
dmitrykrtv <dmitry.korotayev@gmail.com>
2023-03-18 15:57:58 +0300

    feat: new katas

---
+++ Java/8_kyu/Convert a Boolean to a String/index.java
@@ -0,0 +1,13 @@
+//We need a function that can transform a number (integer) into a string.
+//
+// What ways of achieving this do you know?
+//
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+public class BooleanToString {
+  public static String convert(boolean b) {
+    return Boolean.toString(b);
+  }
+}
