1a7315df0c22d91bdf9f7d1895e21afc5462bb73
Dmitry Korotaev <dmitry.korotayev@gmail.com>
2023-06-28 09:15:55 +0300

    feat: new katas

--- Js/6_kyu/Two Sum/index.js
+++ Js/6_kyu/Two Sum/index.js
@@ -1,3 +1,5 @@
+//URL:https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/javascript
+
 //Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
 
 //For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
